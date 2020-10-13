83d5efd7643f612250d5e70390289cb6b74ebf82
KcRaymond <kaseyleigh1978@gmail.com>
2020-10-13 17:09:55 -0600

    added content to html.index, styles.css, index.html - added variables and first prompts

---
+++ script.js
@@ -0,0 +1,29 @@
+
+//Assignment Code + Event Listener to prompt questions when button pushed
+document.querySelector("#generate").addEventListener("click", writePassword);
+
+// Various Arrays 
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["!", "%", "&", ",", "*", "+", "/", "<", ">", "?","~"];
+var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+// Variable Declaration 
+var confirmLength = "";
+var confirmSpecialCharacter;
+var confirmNumericCharacter;
+var confirmUpperCase;
+var confirmLowerCase;
+
+// Prompt to confirm how many characters the user would like in their password
+function generatePassword() {
+  var confirmLength = (prompt("How many characters would you like your password to contain? \nChoose from 8 to 128 characters."));
+
+ 
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
